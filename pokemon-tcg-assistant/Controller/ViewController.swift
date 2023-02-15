885919dac45cb26a51f2794cc3bfb088cbaa4efa
Gabriel Santiago <gaaasts@gmail.com>
2023-02-15 15:03:49 -0300

    first commit

---
+++ pokemon-tcg-assistant/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  pokemon-tcg-assistant
+//
+//  Created by Gabriel Santiago on 15/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ pokemon-tcg-assistantUITests/pokemon_tcg_assistantUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pokemon_tcg_assistantUITestsLaunchTests.swift
+//  pokemon-tcg-assistantUITests
+//
+//  Created by Gabriel Santiago on 15/02/23.
+//
+
+import XCTest
+
+final class pokemon_tcg_assistantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
